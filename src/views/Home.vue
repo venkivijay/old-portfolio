f2e5fa98959f350b9d859427c4d98a760b00c49a
venkivijay <venki96@hotmail.com>
2020-09-03 12:35:51 +0530

    👾 Initial Commit (scaffold)

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="text-black">This commit is just a barebone with all the required configurations</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
